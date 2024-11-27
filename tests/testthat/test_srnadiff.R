6df1ea2eb50442359babd5db7bb2d253d89b587d
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2024-11-27 17:41:52 +0100

    Correct test

--- tests/testthat/test_srnadiff.R
+++ tests/testthat/test_srnadiff.R
@@ -1,63 +1,58 @@
 context("Checking main functions")
 
 exp <- srnadiffExample()
-exp <- srnadiff(exp)
 
 test_that("Testing regions method", {
+    exp <- srnadiff(exp)
     expect_equal(length(regions(exp)), 26)
 })
 
 test_that("Running with different strategies", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, segMethod=c("annotation", "hmm"))
-    expect_equal(length(regions(exp2)), 176)
+    exp <- srnadiff(exp, segMethod=c("annotation", "hmm"))
+    expect_equal(length(regions(exp)), 176)
 })
 
 test_that("Running with different sizes", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, useParameters=list(minSize=15, maxSize=30))
-    expect_equal(length(regions(exp2)), 26)
+    exp <- srnadiff(exp, useParameters=list(minSize=15, maxSize=30))
+    expect_equal(length(regions(exp)), 26)
 })
 
 test_that("Running with different minimum depth", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, useParameters=list(minDepth=1))
-    expect_equal(length(regions(exp2)), 26)
+    exp <- srnadiff(exp, useParameters=list(minDepth=1))
+    expect_equal(length(regions(exp)), 26)
 })
 
 test_that("Running with different transition probabilities", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, segMethod="hmm",
+    exp <- srnadiff(exp, segMethod="hmm",
                      useParameters=list(noDiffToDiff=0.5,diffToNoDiff=0.5))
     expect_equal(length(ranges), 1)
 })
 
 test_that("Running with different emission probabilities", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, segMethod="hmm", useParameters=list(emission=0.75))
-    expect_equal(length(regions(exp2)), 16)
+    exp <- srnadiff(exp, segMethod="hmm", useParameters=list(emission=0.75))
+    expect_equal(length(regions(exp)), 16)
 })
 
 test_that("Running with different emission threshold", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, segMethod="hmm",
+    exp <- srnadiff(exp, segMethod="hmm",
                      useParameters=list(emissionThreshold=0.5))
-    expect_equal(length(regions(exp2)), 16)
+    expect_equal(length(regions(exp)), 16)
 })
 
 test_that("Running with different number of overlapping base pairs", {
-    exp2 <- srnadiffExample()
-    exp2 <- srnadiff(exp2, segMethod="hmm", useParameters=list(minOverlap=15))
-    expect_equal(length(regions(exp2)), 16)
+    exp <- srnadiff(exp, segMethod="hmm", useParameters=list(minOverlap=15))
+    expect_equal(length(regions(exp)), 16)
 })
 
 test_that("Running several threads", {
+    exp <- srnadiff(exp)
     exp2 <- srnadiffExample()
     exp2 <- srnadiff(exp2, nThreads=2)
     expect_equal(regions(exp), regions(exp2))
 })
 
 test_that("Running main function", {
+    exp <- srnadiff(exp)
     expect_equal(length(regions(exp)), 26)
 })
 
